dc1bfd92081e7538bc03b54bab2c65a76fa027dd
Jay Yang <jay@ip-10-161-17-40.us-west-1.compute.internal>
2013-08-04 21:38:40 +0000

    add website push request permission

--- app.js
+++ app.js
@@ -5,14 +5,13 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -31,7 +30,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ public/javascripts/websitepush.js
@@ -0,0 +1,32 @@
+function querypermission() {
+    // Ensure that the user can receive OS X Website Push Notifications.
+    if ('safari' in window && 'pushNotification' in window.safari) {
+        var permissionData = window.safari.pushNotification.permission('web.com.digby.test.jaysorc');
+        checkRemotePermission(permissionData);
+    }
+};
+ 
+var checkRemotePermission = function (permissionData) {
+    if (permissionData.permission === 'default') {
+	console.log('pushNotification.permission: default');
+        // This is a new web service URL and its validity is unknown.
+        var ret = window.safari.pushNotification.requestPermission(
+            //'https://api.zhangxianli.cn/v1/pushPackages/web.com.digby.test.jaysorc', // The web service URL.
+            'https://api.zhangxianli.cn', // The web service URL.
+            'web.com.digby.test.jaysorc',     // The Website Push ID.
+            {'userId': 'jyang@digby.com'}, // Data that you choose to send to your server to help you identify the user.
+            checkRemotePermission         // The callback function.
+        );
+	console.log(JSON.stringify(ret));
+    }
+    else if (permissionData.permission === 'denied') {
+        // The user said no.
+        console.log('pushNotification.permission: denied');
+    }
+    else if (permissionData.permission === 'granted') {
+        // The web service URL is a valid push provider, and the user said yes.
+        // permissionData.deviceToken is now available to use.
+        console.log('pushNotification.permission: granted');
+    }
+};
+
